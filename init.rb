5ed255770ee5935b0d63978eef063e1cf6a3f36c
mikrob <mrobert@octo.com>
2013-11-19 10:17:45 +0100

    upgrade

--- init.rb
+++ init.rb
@@ -1,3 +1,8 @@
 require 'redmine'
 require 'redmine_crowd'
 require 'crowd_client'
+
+
+Redmine::Plugin.register :redmine_crowd_plugin do
+  settings :default => {'empty' => true}, :partial => 'settings'
+end
